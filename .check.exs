0c02a540bb090670e07754c23b533337eeb88152
Cλstor <ygorcastor@gmail.com>
2026-07-10 13:36:50 +0200

    chore: Fix Dialyze and ignoring mix audit on hackney
    Its on testcontainers only

--- .check.exs
+++ .check.exs
@@ -1,5 +1,6 @@
 [
   tools: [
-    {:mix_audit, "mix deps.audit --ignore-advisory-ids GHSA-g2wm-735q-3f56"}
+    {:mix_audit,
+     "mix deps.audit --ignore-advisory-ids GHSA-gp9c-pm5m-5cxr,GHSA-j9wq-vxxc-94wf,GHSA-mp55-p8c9-rfw2,GHSA-pj7v-xfvx-wmjq"}
   ]
 ]

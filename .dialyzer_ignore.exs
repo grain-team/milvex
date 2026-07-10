a0805a2dd43253a37e67f2d0f9be48b18e43c177
Cλstor <ygorcastor@gmail.com>
2026-07-10 13:36:50 +0200

    chore: Fix Dialyze and ignoring mix audit on hackney
    Its on testcontainers only

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -3,7 +3,8 @@
   # dialyzer cannot resolve the dynamic load-balancer dispatch in
   # GRPC.Client.Connection.pick_channel/2, so it treats the `{:ok, channel}`
   # branch of establish_connection/1 (and everything downstream of it) as dead
-  # code. {"lib/milvex/connection.ex", :pattern_match, {145, 7}}
+  # code.
+  {"lib/milvex/connection.ex", :pattern_match, {145, 7}},
   {"lib/milvex/connection.ex", :pattern_match, {168, 7}},
   {"lib/milvex/connection.ex", :pattern_match, {273, 7}},
   {"lib/milvex/connection.ex", :pattern_match, {314, 13}},

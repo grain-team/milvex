993d95a24dc60d7de206e847a2a599041a3c5756
Cλstor <ygorcastor@gmail.com>
2026-07-31 14:43:18 +0200

    chore: Ignoring advisories we can't fix

--- .check.exs
+++ .check.exs
@@ -1,6 +1,6 @@
 [
   tools: [
     {:mix_audit,
-     "mix deps.audit --ignore-advisory-ids GHSA-gp9c-pm5m-5cxr,GHSA-j9wq-vxxc-94wf,GHSA-mp55-p8c9-rfw2,GHSA-pj7v-xfvx-wmjq"}
+     "mix deps.audit --ignore-advisory-ids GHSA-gp9c-pm5m-5cxr,GHSA-j9wq-vxxc-94wf,GHSA-mp55-p8c9-rfw2,GHSA-pj7v-xfvx-wmjq,GHSA-w4f7-4cxr-rv3c"}
   ]
 ]
